4c8b4baeff346552a6480a4d70f5f8c624a85e9c
AlekseyF3 <56193692+AlekseyF3@users.noreply.github.com>
2019-10-30 17:52:36 +0200

    Add files via upload

---
+++ Laba#3_import_and_from.py
@@ -0,0 +1,15 @@
+from contant import g, M, R, p, k
+from math import pi, sqrt
+from math import e
+h = 100
+a = pi/3
+v = sqrt((g*h*pi)/(2*M*R))
+print(v)
+T = 200
+o = 300
+N = 2/(sqrt(pi)) * p/((k*T)**3/2) * e**((-e)/(k*T)) 
+
+print(N)
+
+
+
