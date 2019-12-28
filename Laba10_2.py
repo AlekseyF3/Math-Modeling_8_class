359a48fa00ec9e4a6a99cf4a748d4f6d06a6fc27
AlekseyF3 <56193692+AlekseyF3@users.noreply.github.com>
2019-12-28 16:17:44 +0200

    Add files via upload

--- Laba10_2.py
+++ Laba10_2.py
@@ -4,14 +4,15 @@ from scipy.integrate import odeint
 
 t = np.arange(0, 20, 0.01)
 g = 9.8
-k = 6
+k = 2
 def diff_func(z,t):
     s, v = z
     ds_dt = v
-    dv_dt = -g*np.sin(s/l) - k/v *v
+    dv_dt = -g*np.sin(s/l) - k/m *v
     return ds_dt,dv_dt
 
 l = 5
+m = 10
 s0 = 1
 v0 = 1
 z0 = s0,v0
