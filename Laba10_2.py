789982395c29e98499ef360f40d27cd06b932727
AlekseyF3 <56193692+AlekseyF3@users.noreply.github.com>
2019-12-28 16:14:30 +0200

    Add files via upload

---
+++ Laba10_2.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+
+t = np.arange(0, 20, 0.01)
+g = 9.8
+k = 6
+def diff_func(z,t):
+    s, v = z
+    ds_dt = v
+    dv_dt = -g*np.sin(s/l) - k/v *v
+    return ds_dt,dv_dt
+
+l = 5
+s0 = 1
+v0 = 1
+z0 = s0,v0
+
+sol = odeint(diff_func,z0,t)
+plt.plot(t,sol[:,0], 'b')
+
+    
